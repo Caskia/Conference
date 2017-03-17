4ba8f99ea5da89d8e4b1a4f14468aecb6eec8805
Caskia <liubai61@163.com>
2017-03-17 21:29:08 +0800

    add mysql store

---
+++ Enode.Store.Mysql/Configurations/Configuration.cs
@@ -0,0 +1,29 @@
+﻿using ENode.Configurations;
+using ENode.Eventing;
+using ENode.Infrastructure;
+using Sparxo.Enode.Eventing.Impl;
+using Sparxo.Enode.Infrastructure.Impl;
+
+namespace Enode.Store.Mysql.Configurations
+{
+    public static class ENodeConfigurationExtension
+    {
+        public static ENodeConfiguration UseMySqlEventStore(this ENodeConfiguration enodeConfiguration, OptionSetting optionSetting = null)
+        {
+            enodeConfiguration.GetCommonConfiguration().SetDefault<IEventStore, MySqlEventStore>(new MySqlEventStore(optionSetting));
+            return enodeConfiguration;
+        }
+
+        public static ENodeConfiguration UseMySqlLockService(this ENodeConfiguration enodeConfiguration, OptionSetting optionSetting = null)
+        {
+            enodeConfiguration.GetCommonConfiguration().SetDefault<ILockService, MySqlLockService>(new MySqlLockService(optionSetting));
+            return enodeConfiguration;
+        }
+
+        public static ENodeConfiguration UseMySqlPublishedVersionStore(this ENodeConfiguration enodeConfiguration, OptionSetting optionSetting = null)
+        {
+            enodeConfiguration.GetCommonConfiguration().SetDefault<IPublishedVersionStore, MySqlPublishedVersionStore>(new MySqlPublishedVersionStore(optionSetting));
+            return enodeConfiguration;
+        }
+    }
+}
---
+++ Enode.Store.Mysql/Infrastructure/Impl/MySqlLockService.cs
@@ -0,0 +1,90 @@
+﻿using Dapper;
+using ECommon.Utilities;
+using ENode.Configurations;
+using ENode.Infrastructure;
+using MySql.Data.MySqlClient;
+using Sparxo.Dapper;
+using System;
+using System.Data;
+using System.Linq;
+
+namespace Sparxo.Enode.Infrastructure.Impl
+{
+    public class MySqlLockService : ILockService
+    {
+        #region Private Variables
+
+        private readonly string _connectionString;
+        private readonly string _lockKeySqlFormat;
+        private readonly string _tableName;
+
+        #endregion Private Variables
+
+        #region Constructors
+
+        public MySqlLockService(OptionSetting optionSetting)
+        {
+            if (optionSetting != null)
+            {
+                _connectionString = optionSetting.GetOptionValue<string>("ConnectionString");
+                _tableName = optionSetting.GetOptionValue<string>("TableName");
+            }
+            else
+            {
+                var setting = ENodeConfiguration.Instance.Setting.DefaultDBConfigurationSetting;
+                _connectionString = setting.ConnectionString;
+                _tableName = setting.LockKeyTableName;
+            }
+
+            Ensure.NotNull(_connectionString, "_connectionString");
+            Ensure.NotNull(_tableName, "_tableName");
+
+            _lockKeySqlFormat = "SELECT * FROM " + _tableName + " WHERE `Name` = '{0}' FOR UPDATE";
+        }
+
+        #endregion Constructors
+
+        public void AddLockKey(string lockKey)
+        {
+            using (var connection = GetConnection())
+            {
+                var count = connection.QueryList(new { Name = lockKey }, _tableName).Count();
+                if (count == 0)
+                {
+                    connection.Insert(new { Name = lockKey }, _tableName);
+                }
+            }
+        }
+
+        public void ExecuteInLock(string lockKey, Action action)
+        {
+            using (var connection = GetConnection())
+            {
+                connection.Open();
+                var transaction = connection.BeginTransaction();
+                try
+                {
+                    LockKey(transaction, lockKey);
+                    action();
+                    transaction.Commit();
+                }
+                catch
+                {
+                    transaction.Rollback();
+                    throw;
+                }
+            }
+        }
+
+        private IDbConnection GetConnection()
+        {
+            return new MySqlConnection(_connectionString);
+        }
+
+        private void LockKey(IDbTransaction transaction, string key)
+        {
+            var sql = string.Format(_lockKeySqlFormat, key);
+            transaction.Connection.Query(sql, transaction: transaction);
+        }
+    }
+}
